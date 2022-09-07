bb798c810d29553437cf6f3cbd634ed4e2f825c5
Angelica Sotis <angelica.sotis9@gmail.com>
2022-09-07 10:10:11 +0800

    third commit

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 const port = process.env.PORT||8080;
+const readline = require('readline');
+const { syncBuiltinESMExports } = require('module');
+
 
 //attach http server to the socket.io
 const io = require('socket.io')(http);
@@ -16,22 +19,60 @@ app.get('/', (req, res) => {
 //create connection
 io.on('connection', socket => {
     
-    setInterval(function() {
-        const fileContent = fs.createReadStream("example/data.txt", 'utf8');
-        /**
-         * socket.send(fileContent);
-        console.log(`sending: %s`, fileContent)
-         */
-        fileContent.on('data', function(chunk){
-            //response.write(chunk);
-            socket.send(chunk);
-            console.log(chunk);
-            }, 200);
-      }, 500);
+    
 
-      
-})
+    async function processLineByLine(){
+
+        const filestream = fs.createReadStream("example/data.txt");
+
+        const rl = readline.createInterface ({
+            input: filestream,
+            crlfDelay: Infinity
+        });
+    
+        for await(const line of rl) {
+            console.log(`${line}`);
+            socket.send(line);
+            await sleep(1000)
+        }
+
+        function sleep(ms){
+            return new Promise(resolve=>{
+                setTimeout(resolve,ms)
+            })
+        }
+        
+    }
+
+    processLineByLine();
+
+
+    
+}, 500);
+
 
 http.listen(port, () => {
     console.log(`App Listening to port ${port}`);
 })
+
+
+/**
+ * const fileContent = fs.createReadStream("example/data.js", 'utf8');
+       
+        socket.send(fileContent);
+        console.log(`sending: %s`, fileContent)
+
+        fileContent.on('data', function(chunk){
+            console.log(chunk);
+        response.write(chunk);
+            
+            for (let i = 0; i < chunk.length; i++) {
+                socket.send(chunk);
+                console.log(chunk);
+            }
+            
+            socket.send(chunk);
+            console.log(chunk);
+         }, 200)
+ * 
+ */
